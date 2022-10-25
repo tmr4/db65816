98a41d323487147a7df2a9f8c9ff798576c38611
tmrob4 <tmrob4@gmail.com>
2022-10-25 09:46:40 -0700

    Comments and cleanup

--- src/ee65xx.ts
+++ src/ee65xx.ts
@@ -1,47 +1,13 @@
 /* eslint-disable eqeqeq */
 
-//import * as vscode from 'vscode';
-//import * as vscode from '@vscode/debugadapter';
 import { EventEmitter } from 'events';
 import * as fs from 'fs';
-import * as path from 'path';
-
-//import { RuntimeVariable, IRegisters65816 } from './da65xx'
 
 import { MPU65816 } from './mpu65816';
 import { ObsMemory } from './obsmemory';
 import { Interrupts } from './interrupts';
 import { terminalStart, terminalDispose, terminalWrite } from './terminal';
 import { Debug65xxSession } from './da65xx';
-
-export interface IRuntimeBreakpoint {
-    id: number;
-    line: number;
-    address: number;
-    verified: boolean;
-}
-
-interface IRuntimeStackFrame {
-    index: number;
-    name: string;
-    file: string;
-    line: number;
-    column: number;
-    instruction: number;
-//    instructionPointerReference?: string;
-}
-
-interface IRuntimeStack {
-    count: number;
-    frames: IRuntimeStackFrame[];
-}
-
-interface IPosition {
-    line: number;
-    address: number;
-    opcode: number;
-    instruction: string;
-}
 
 var buf = Buffer.alloc(0);
 
@@ -66,31 +32,33 @@ function putc(value: number): void {
 function getc(value: number): void {
 }
 
-/**
- * EE65xx is a 65xx execution engine with debugging support:
- * it "executes" a 65xx binary and sends events to the debug adapter informing it
- * of key debugging events.  The debug adapter "follows along" with a CA65 listing
- * file (*.lst), simulating "running" through the source code line-by-line.  EE65xx
- * exposes several methods allowing the debug adapter to control the simulation.
- * EE65xx supports typical stepping and breakpoint functionality as the core of the
- * "debugging support".  
- * EE65xx is completely independent from VS Code and the 
- * Debug Adapter and can be run as a standalone simulator without debugging.
- */
+// EE65xx is a 65xx execution engine with debugging support.
+// it "executes" a 65xx binary and sends events to the debug adapter informing it
+// of key debugging events.  The debug adapter "follows along" with a CA65 listing
+// file (*.lst), simulating "running" through the source code line-by-line.  EE65xx
+// exposes several methods allowing the debug adapter to control the simulation.
+// EE65xx supports typical stepping and breakpoint functionality as the core of the
+// "debugging support".  
+// EE65xx is completely independent from VS Code and the 
+// Debug Adapter and can be run as a standalone simulator without debugging.
 export class EE65xx extends EventEmitter {
 
     private continueID!: NodeJS.Timeout;
     private isBreak: boolean = false;
-    private isDebug: boolean = true;
+    private isDebug!: boolean;
     private da65xx: Debug65xxSession;
 
 //    private filename: string = '';
 
-    dbInt!: Interrupts;
+    private dbInt!: Interrupts;
 //    aciaAddr!: number;
 //    viaAddr!: number;
 
     public mpu!: MPU65816;
+
+    //private getcAddr: number;
+    //private putcAddr: number;
+    public obsMemory!: ObsMemory;
 
     public constructor(da65xx: Debug65xxSession) {
         super();
@@ -100,15 +68,16 @@ export class EE65xx extends EventEmitter {
         this.da65xx = da65xx;
     }
 
-    //private getcAddr: number;
-    //private putcAddr: number;
-    public obsMemory!: ObsMemory;
-    
+
+    // *******************************************************************************************
+    // public program control methods
+
     // Start executing the given program
     public start(bsource: string = '', fsource: string = '', aciaAddr: number | undefined, viaAddr: number | undefined, stopOnEntry: boolean = true, debug: boolean = true): void {
 
         terminalStart('65816 Debug', viaAddr ? true : false); // start debug terminal if not already started
-
+        this.isDebug = debug;
+        
         this.loadBinary(bsource);
 
         this.mpu = new MPU65816(this.obsMemory.obsMemory);
@@ -147,72 +116,25 @@ export class EE65xx extends EventEmitter {
         }
     }
 
-    // *** TODO: terminal isn't disposed when debugging is stopped ***
     public end() {
         terminalDispose();
         this.sendEvent('exited');
     }
 
     // Continue execution to address or until we hit a breakpoint
-    // Returns false on hitting a breakpoint, otherwise returns true
     public stepTo(address: number) {
 
         // take a single step to get over a breakpoint
         this.step(false);
 
         while (address !== (this.mpu.pc + (this.mpu.pbr << 16))) {
-            if (this.da65xx.checkBP()) {
-//                this.sendEvent('stopOnBreakpoint');
+            if (this.da65xx?.checkBP()) {
                 return;
             }
             this.step(false);
         }
         this.sendEvent('stopOnStep');
     }
-
-    // Returns a stacktrace for the current line
-//    public stack(): IRuntimeStack {
-//
-//        const frames: IRuntimeStackFrame[] = [];
-//        const stackFrame: IRuntimeStackFrame = {
-//            index: 1,
-////            name: pl.instruction,                    // routine label
-//            name: '',
-//            file: this.filename,
-////            line: this.currentLine,
-//            line: 0,
-//            column: 0, // words[i].index
-//            instruction: this.mpu.pc
-//        };
-//
-//        frames.push(stackFrame);
-//
-//        return {
-//            frames: frames,
-//            count: 1
-//        };
-//    }
-
-    // load binary and initialize memory as observable
-    private loadBinary(file: string): void {
-        this.obsMemory = new ObsMemory(fs.readFileSync(file));
-    }
-
-//  ; Direct page(DP) defines:
-//      x = sforth instance # 0-4
-//      DPx:  direct page start
-//      DP0 = __DP0_START__
-//  
-//      ; Offsets into the direct page applicable to each Forth instance
-//      ; SPo:  Top of hardware return stack
-//      ; FSPo: Top of Forth data stack
-//      ; RSPo: Top of Forth return stack
-//      ; *** TODO: these need tuned.We don't need that much return stack.
-//      ;       Maybe a space for a future control or floating point stack ? ***
-//  SPo   = __DP0_SIZE__ - 1
-//  FSPo = __DP0_SIZE__ / 3 * 2
-//  RSPo = __DP0_SIZE__ / 3
-//  FPSPo = __DP0_SIZE__ / 3 - $800
 
     // Execute the current line.
     // Returns true if execution sent out a stopped event and needs to stop.
@@ -245,6 +167,15 @@ export class EE65xx extends EventEmitter {
         this.continueID = setInterval(() => { this.run(); }, 10);
     }
 
+
+    // *******************************************************************************************
+    // private helper methods
+
+    // load binary and initialize memory as observable
+    private loadBinary(file: string): void {
+        this.obsMemory = new ObsMemory(fs.readFileSync(file));
+    }
+
     // run source code for a given number of steps
     private run() {
         let count = 0;
@@ -258,12 +189,9 @@ export class EE65xx extends EventEmitter {
         this.step(false);
 
         while (count++ < steps && !this.isBreak) {
-            if (this.isDebug && this.da65xx.checkBP()) {
+            if (this.isDebug && this.da65xx?.checkBP()) {
                 clearInterval(this.continueID);
                 this.isBreak = true;     // force step loop to exit
-                // send 'stopped' event
-                //                console.log('Hit breakpoint at: ' + bps[0].address);
-//                this.sendEvent('stopOnBreakpoint');
                 break;
             } else if (this.mpu.waiting && (steps === 100000)) {
                 // reduce steps if we've shifted to waiting during run loop
@@ -277,51 +205,11 @@ export class EE65xx extends EventEmitter {
         this.isBreak = false;
     }
 
-    public sendEvent(event: string, ...args: any[]): void {
+    private sendEvent(event: string, ...args: any[]): void {
         setTimeout(() => {
             this.emit(event, ...args);
         }, 0);
     }
 
-    public getVariable(name: string, address: number) {
-
-    }
-
-//    public readMemory(address: number, length: number): RuntimeVariable[] {
-//        let rv: RuntimeVariable[] = [];
-//        let end = address + length;
-//
-//        for (let i = address; i < end; i++) {
-//            rv.push(new RuntimeVariable(`global_${i}`, i));
-//        }
-//        return rv;
-//    }
-    public readMemory(address: number, length: number): Uint8Array {
-        return this.obsMemory.memory.slice(address,address+length);
-    }
 }
-
-// registered listener
-// from: https://stackoverflow.com/questions/1759987/listening-for-variable-changes-in-javascript
-// 
-// let x = {
-//   aInternal: 10,
-//   aListener: function(val: any) {},
-//   set a(val) {
-//     this.aInternal = val;
-//     this.aListener(val);
-//   },
-//   get a() {
-//     return this.aInternal;
-//   },
-//   registerListener: function(listener: any) {
-//     this.aListener = listener;
-//   }
-// };
-// 
-// x.registerListener(function(val: any) {
-//   console.log(String.fromCharCode(val));
-// });
-// 
-// x.a = 0x65;
 
